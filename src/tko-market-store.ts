d756fb75d07173c09f305b18c00a082d6bbb695b
sharafutdinov <asharafutdinov@tko-inform.ru>
2023-04-11 11:00:55 +0300

    add orderService

--- src/tko-market-store.ts
+++ src/tko-market-store.ts
@@ -1,3 +1,18 @@
 import { BehaviorSubject } from "rxjs";
 
-export const orders$ = new BehaviorSubject([]);
+const order$ = new BehaviorSubject(new Set());
+
+export const orderService$ = {
+  addProduct: (product) => {
+    const currentValue = order$.value;
+
+    if (currentValue.has(product)) {
+      currentValue.delete(product);
+    } else {
+      currentValue.add(product);
+    }
+    order$.next(new Set(currentValue));
+  },
+  clearOrder: () => order$.next(new Set<any>()),
+  getOrder: () => order$.asObservable(),
+};
